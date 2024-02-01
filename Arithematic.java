7420359d42c8c158c4d92054ada6a04dc945284c
aniketsunilwalgude <158460673+aniketsunilwalgude@users.noreply.github.com>
2024-02-01 17:27:11 +0530

    Add files via upload

--- Arithematic.java
+++ Arithematic.java
@@ -1,11 +1,20 @@
-package Marvellous.Infosystems;
+package Marvellous;
 
 public class Arithematic
 {
-    public int Substraction(int A, int B)
+    public int iValeue1;
+    public int iValeue2;
+
+    public Arithematic(int A, int B)
     {
-        return A-B;
+        this.iValeue1 = A;
+        this.iValeue2 = B;
+    }
+
+    public int Addition()
+    {
+        int iSum = 0;
+        iSum = this.iValeue1 + this.iValeue2;
+        return iSum;
     }
 }
-
-// javac -d . Arithematic.java
